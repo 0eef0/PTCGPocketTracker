c2d5cf4b66db4c891b219fbf7229ffd714a93053
Ethan Roldan <ethanryanroldan@gmail.com>
2024-12-15 22:03:30 -0700

    rearranged deck editor

--- TCGP Tracker/DeckEditor.swift
+++ TCGP Tracker/DeckEditor.swift
@@ -20,16 +20,17 @@ struct DeckEditor: View {
         ScrollView {
             ForEach(0..<20, id: \.self) { index in
                 HStack {
-                    TextField("Card \(index + 1)", value: $deck.deckListIDs[index], formatter: formatter)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .keyboardType(.decimalPad)
-                    
                     Picker(selection: $deck.deckListExpansions[index], label: Text("Expansion")) {
                         Text("[Expansion unset]").tag("")
                         Text("Promo").tag("PromoA")
                         Text("Genetic Apex").tag("GeneticApex")
                     }
-                    .frame(width: UIScreen.main.bounds.width / 2)
+                    .frame(width: UIScreen.main.bounds.width / 2.25)
+                    
+                    TextField("Card \(index + 1)", value: $deck.deckListIDs[index], formatter: formatter)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .keyboardType(.decimalPad)
+                        .frame(width: UIScreen.main.bounds.width / 2.25)
                 }
             }
         }
