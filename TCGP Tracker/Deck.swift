c380abffc6207ada925a03558f7b4f32e028e71a
Ethan Roldan <ethanryanroldan@gmail.com>
2024-12-15 20:13:03 -0700

    imported all images (for now), adding image functionality

--- TCGP Tracker/Deck.swift
+++ TCGP Tracker/Deck.swift
@@ -42,22 +42,3 @@ final class Deck {
         }
     }
 }
-//
-//class Card {
-//    var cardName:String
-//    var cardExpansion:String
-//    
-//    init(cardName: String, cardExpansion: String) {
-//        self.cardName = cardName
-//        self.cardExpansion = cardExpansion
-//    }
-//    
-//    func getExpansionColor() -> Color {
-//        switch(cardExpansion) {
-//            case "geneticApex":
-//                return .purple
-//            default:
-//                return .black
-//        }
-//    }
-//}
