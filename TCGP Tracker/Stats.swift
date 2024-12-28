bf763f996d066ed5548e12f81a42a5b2ac5b1ff9
Ethan Roldan <ethanryanroldan@gmail.com>
2024-12-28 12:42:50 -0700

    added best and worst deck display in stats view

--- TCGP Tracker/Stats.swift
+++ TCGP Tracker/Stats.swift
@@ -9,10 +9,12 @@ import SwiftUI
 import SwiftData
 
 struct Stats: View {
-    @State var decks:[Deck]
+    @State var decks: [Deck]
     
-    @State var wins = 0;
-    @State var losses = 0;
+    @State var bestDeck: Deck = Deck(deckName: "", wins: -1, losses: 1)
+    @State var worstDeck: Deck = Deck(deckName: "", wins: -1, losses: 1)
+    @State var wins = 0
+    @State var losses = 0
     
     var body: some View {
         NavigationStack {
@@ -47,18 +49,66 @@ struct Stats: View {
                     .fontWeight(.bold)
                 
                 Spacer()
+                
+                let bestWinLossRatio = Double(bestDeck.wins) / Double(bestDeck.losses)
+                let worstWinLossRatio = Double(worstDeck.wins) / Double(worstDeck.losses)
+
+                let bestRatioString = String(format: "%.2f", bestWinLossRatio)
+                let worstRatioString = String(format: "%.2f", worstWinLossRatio)
+
+                Text("Best Deck: \(bestDeck.deckName) (\(bestRatioString))")
+                    .font(.body)
+                    .fontWeight(.bold)
+
+                Text("Worst Deck: \(worstDeck.deckName) (\(worstRatioString))")
+                    .font(.body)
+                    .fontWeight(.bold)
+                
+                Spacer()
             }
             .navigationTitle("Overall Stats")
             
             Spacer()
         }
-        .presentationDetents([.height(275)])
+        .presentationDetents([.height(325)])
         .presentationDragIndicator(.visible)
         .onAppear {
+            var totalWins = 0
+            var totalLosses = 0
+            var localBestDeck: Deck? = nil
+            var localWorstDeck: Deck? = nil
+            
             for deck in decks {
-                wins += deck.wins
-                losses += deck.losses
+                totalWins += deck.wins
+                totalLosses += deck.losses
+                
+                let currentDeckRatio = Double(deck.wins) / Double(deck.losses)
+                
+                if let best = localBestDeck {
+                    if currentDeckRatio > Double(best.wins) / Double(best.losses) {
+                        localBestDeck = deck
+                    }
+                } else {
+                    localBestDeck = deck
+                }
+                
+                if let worst = localWorstDeck {
+                    if currentDeckRatio < Double(worst.wins) / Double(worst.losses) {
+                        localWorstDeck = deck
+                    }
+                } else {
+                    localWorstDeck = deck
+                }
             }
+            
+            if let bestDeck = localBestDeck {
+                self.bestDeck = bestDeck
+            }
+            if let worstDeck = localWorstDeck {
+                self.worstDeck = worstDeck
+            }
+            self.wins = totalWins
+            self.losses = totalLosses
         }
     }
 }
